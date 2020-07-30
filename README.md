d287a018a60bc11cf93df30f0a52b660526ffe4c
Peter Cotton <petercotton@Peters-Mac-mini.local>
2020-07-30 19:28:50 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # microtutorial
-microprediction stream creation tutorial
+## microprediction stream creation tutorial
+
+Illustrates how to tap into a crowd of intelligent people and algorithms to effect live prediction
---
+++ examples/creating_a_key.py
@@ -0,0 +1,6 @@
+from microprediction import new_key
+
+if __name__=="__main__":
+    write_key = new_key(difficulty=12)  # Takes a LONG time, sorry.
+                                        # By all means hit us up for one   info@microprediction.org
+    print(write_key)
